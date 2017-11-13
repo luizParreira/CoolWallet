fd9df2349984b949e0b5f9d8df6b11090b619fa3
Luiz Parreira <luizfilipester@gmail.com>
2017-11-13 10:48:41 -0200

    Made some final adjustments

--- src/state/price/selectors.js
+++ src/state/price/selectors.js
@@ -2,4 +2,4 @@ import {createSelector} from 'reselect';
 
 const price = state => state.price;
 export const usdPrice = createSelector(price, price => price && price.price_usd);
-export const loading = createSelector(state => state, state => state.loading);
+export const loading = state => state.loading;
--- src/state/selectors.js
+++ src/state/selectors.js
@@ -1,6 +1,6 @@
 import {createSelector} from 'reselect';
 import * as priceSelectors from './price/selectors';
 
-export const usdPrice = createSelector(state => state.price, priceSelectors.usdPrice);
-export const loading = createSelector(state => state.price, priceSelectors.loading);
-
+const price = state => state.price;
+export const usdPrice = createSelector(price, priceSelectors.usdPrice);
+export const loading = createSelector(price, priceSelectors.loading);
